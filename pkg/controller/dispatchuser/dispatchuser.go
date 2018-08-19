ebbd6ce93afa22a0f537cf00d67d7904b9c9084c
Hantao (WIll) Wang <hantaowang97@gmail.com>
2018-08-19 14:01:47 -0700

    implement controlelr workflow

--- pkg/controller/dispatchuser/dispatchuser.go
+++ pkg/controller/dispatchuser/dispatchuser.go
@@ -3,15 +3,11 @@ package dispatchuser
 import (
 	"time"
 	"strings"
-
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue"
-	"k8s.io/kubernetes/pkg/controller"
-	"k8s.io/kubernetes/pkg/util/metrics"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-
+	"k8s.io/apimachinery/pkg/api/errors"
 	ownednamespace_lister "github.com/hantaowang/dispatch/pkg/client/listers/ownednamespace/v1"
 	dispatchuser_lister "github.com/hantaowang/dispatch/pkg/client/listers/dispatchuser/v1"
 	dispatchuser_informer "github.com/hantaowang/dispatch/pkg/client/informers/externalversions/dispatchuser/v1"
@@ -19,8 +15,15 @@ import (
 	dispatchuser_api "github.com/hantaowang/dispatch/pkg/apis/dispatchuser/v1"
 
 	"github.com/hantaowang/dispatch/pkg/cmd"
+
 	"log"
 	"fmt"
+
+	informer_v1 "k8s.io/client-go/informers/core/v1"
+	lister_v1 "k8s.io/client-go/listers/core/v1"
+	"k8s.io/kubernetes/pkg/controller"
+
+	dispatchuser "github.com/hantaowang/dispatch/pkg/apis/dispatchuser/v1"
 )
 
 // NamespaceController is responsible for performing actions dependent upon a namespace phase
@@ -33,42 +36,60 @@ type DispatchUserController struct {
 	// lister that can list DispatchUsers from a shared cache
 	duLister dispatchuser_lister.DispatchUserLister
 	onLister ownednamespace_lister.OwnedNamespaceLister
+	saLister lister_v1.ServiceAccountLister
 
 	// returns true when the DispatchUser cache is ready
 	duListerSynced cache.InformerSynced
 	onListerSynced cache.InformerSynced
-
-	// dispatchusers that have been queued up for processing by workers
-	queue workqueue.RateLimitingInterface
+	saListerSynced cache.InformerSynced
 
 	// clients to modify resources
 	clientsets	cmd.ClientSets
+
+	// Buffered channel of events to be done
+	workqueue 	chan DispatchUserEvent
+}
+
+type DispatchUserEvent struct {
+	action		string
+	old			dispatchuser.DispatchUser
+	new			dispatchuser.DispatchUser
 }
 
 // NewNamespaceController creates a new NamespaceController
 func NewDispatchUserController(
 	duInformer	dispatchuser_informer.DispatchUserInformer,
 	onInformer ownednamespace_informer.OwnedNamespaceInformer,
+	saInformer	informer_v1.ServiceAccountInformer,
 	clientSets cmd.ClientSets,
 	) *DispatchUserController {
 
-
-	if clientSets.OriginalClient != nil &&  clientSets.OriginalClient.CoreV1().RESTClient().GetRateLimiter() != nil {
-		metrics.RegisterMetricAndTrackRateLimiterUsage("dispatch-user-controller",
-			clientSets.OriginalClient.CoreV1().RESTClient().GetRateLimiter())
-	}
-
 	duc := &DispatchUserController{
 		GroupVersionKind: dispatchuser_api.SchemeGroupVersion.WithKind("DispatchUser"),
-		clientsets:       clientSets,
-		queue:            workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "dispatchuser"),
+		clientsets: clientSets,
+		workqueue: make(chan DispatchUserEvent, 100),
 	}
 
-
 	duInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		//AddFunc:    duc.enqueueDispatchUser,
-		//UpdateFunc: duc.enqueueDispatchUser,
-		//DeleteFunc: duc.enqueueDispatchUser,
+		AddFunc:    func(obj interface{}) {
+			duc.workqueue <- DispatchUserEvent{
+				action: "add",
+				new: obj.(dispatchuser.DispatchUser),
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			duc.workqueue <- DispatchUserEvent{
+				action: "update",
+				old: oldObj.(dispatchuser.DispatchUser),
+				new: newObj.(dispatchuser.DispatchUser),
+			}
+		},
+		DeleteFunc:    func(obj interface{}) {
+			duc.workqueue <- DispatchUserEvent{
+				action: "delete",
+				old: obj.(dispatchuser.DispatchUser),
+			}
+		},
 	})
 
 	duc.duLister = duInformer.Lister()
@@ -77,19 +98,21 @@ func NewDispatchUserController(
 	duc.onLister = onInformer.Lister()
 	duc.onListerSynced = onInformer.Informer().HasSynced
 
+	duc.saLister = saInformer.Lister()
+	duc.saListerSynced = saInformer.Informer().HasSynced
+
 	return duc
 }
 
 // Run begins watching and syncing.
 func (duc *DispatchUserController) Run(workers int, stopCh <-chan struct{}) {
 	defer utilruntime.HandleCrash()
-	defer duc.queue.ShutDown()
 
 	controllerName := strings.ToLower(duc.Kind)
 	log.Printf("Starting %v controller", controllerName)
 	defer log.Printf("Shutting down %v controller", controllerName)
 
-	if !controller.WaitForCacheSync(duc.Kind, stopCh, duc.duListerSynced, duc.onListerSynced) {
+	if !controller.WaitForCacheSync(duc.Kind, stopCh, duc.duListerSynced, duc.onListerSynced, duc.saListerSynced) {
 		return
 	}
 
@@ -108,24 +131,22 @@ func (duc *DispatchUserController) worker() {
 }
 
 func (duc *DispatchUserController) processNextWorkItem() bool {
-	key, quit := duc.queue.Get()
-	if quit {
+	event := <- duc.workqueue
+
+	var err error
+	if event.action == "add" {
+		err = duc.addHandler(event)
+	} else if event.action == "update" {
+		err = duc.updateHandler(event)
+	} else if event.action == "delete" {
+		err = duc.deleteHandler(event)
+	} else {
+		err = fmt.Errorf("event action not recoginized %s", event.action)
+	}
+
+	if err != nil {
 		return false
 	}
-	defer duc.queue.Done(key)
-
-	err := duc.syncHandler(key.(string))
-	if err == nil {
-		duc.queue.Forget(key)
-		return true
-	}
-
-	utilruntime.HandleError(fmt.Errorf("Sync %q failed with %v", key, err))
-	duc.queue.AddRateLimited(key)
 
 	return true
-}
-
-func (duc *DispatchUserController) syncHandler(x string) error {
-	return nil
 }
